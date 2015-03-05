ccc873295138c8773531907b4dedcfc928292b07
Jesse Gauthier <Jessegauthier070296@gmail.com>
2015-03-05 17:22:34 -0500

    Added Arrow Loot Areas

--- Code4.lua
+++ Code4.lua
@@ -232,3 +232,38 @@ function processPlayers(players)
 end
 
 registerHook("BLOCK_GAINS_CURRENT", "fireTick", "code4", -1, 82, -36);
+
+-- Item Pickups
+--
+
+function ArrowPickUp1(data)
+	local player = Player:new(data["player"]);
+	player:addItem(262, 10, 1);
+	player:sendMessage("&eYou walked over a dead body, you looted 1 Arrow.");
+end
+
+registerHook("REGION_ENTER", "ArrowPickUp1", "Code4-arrowpickup1");
+
+function ArrowPickUp2(data)
+	local player = Player:new(data["player"]);
+	player:addItem(262, 10, 1);
+	player:sendMessage("&eYou walked over a dead body, you looted 1 Arrow.");
+end
+
+registerHook("REGION_ENTER", "ArrowPickUp2", "Code4-arrowpickup2");
+
+function ArrowPickUp3(data)
+	local player = Player:new(data["player"]);
+	player:addItem(262, 10, 1);
+	player:sendMessage("&eYou walked over a dead body, you looted 1 Arrow.");
+end
+
+registerHook("REGION_ENTER", "ArrowPickUp3", "Code4-arrowpickup3");
+
+function ArrowPickUp4(data)
+	local player = Player:new(data["player"]);
+	player:addItem(262, 10, 1);
+	player:sendMessage("&eYou walked over a dead body, you looted 1 Arrow.");
+end
+
+registerHook("REGION_ENTER", "ArrowPickUp4", "Code4-arrowpickup4");

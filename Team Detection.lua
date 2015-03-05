cc8f5650c9a20d45409b71b1d73cbc4f2fe083c4
Jesse Gauthier <Jessegauthier070296@gmail.com>
2015-03-05 18:47:19 -0500

    New Format for team finding

--- Team Detection.lua
+++ Team Detection.lua
@@ -1,16 +1,14 @@
 -- Create a new array.
 local blueTeamPlayers = {};
 
--- Create a player.
-local myPlayer = Player:new('Centralan');
+function addPlayerToBlueTeam(playerName)
+	table.insert(blueTeamPlayers, Player:new(playerName));
+end
 
--- Add the player to the array.
-table.insert(blueTeamPlayers, myPlayer);
+addPlayerToBlueTeam('Centralan');
+addPlayerToBlueTeam('Kruithne');
 
 -- Loop all the players in the array and send them a message.
 for index, player in pairs(blueTeamPlayers) do
 	player:sendMessage('Hi');
 end
-
--- Clear the array.
-blueTeamPlayers = {};

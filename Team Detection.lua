9436aca997651c3a877872c5ec6bfc89d4b6985d
Jesse Gauthier <Jessegauthier070296@gmail.com>
2015-03-05 18:43:32 -0500

    Create Team Detection.lua

---
+++ Team Detection.lua
@@ -0,0 +1,17 @@
+-- Create a new array.
+local blueTeamPlayers = {};
+
+-- Create a player.
+local myPlayer = Player:new('Centralan');
+local myPlayer = Player:new('Kruithne');
+
+-- Add the player to the array.
+table.insert(blueTeamPlayers, myPlayer);
+
+-- Loop all the players in the array and send them a message.
+for index, player in pairs(blueTeamPlayers) do
+	player:sendMessage('Hi');
+end
+
+-- Clear the array.
+blueTeamPlayers = {};

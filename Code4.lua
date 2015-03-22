7e406379fd157a1d272a9878659ee87b2e95705e
Jesse Gauthier <Jessegauthier070296@gmail.com>
2015-03-21 20:01:23 -0400

    Update Code4.lua

--- Code4.lua
+++ Code4.lua
@@ -45,7 +45,7 @@ end
 
 function chatMonitor(data)
 	-- Make sure it's you giving the command.
-	if data.player == "Centralan" or data.player == "iliketoeatpenuts" then or data.player == "Kruithne" then
+	if data.player == "Centralan" or data.player == "iliketoeatpenuts" or data.player == "Kruithne" then
 		local player = Player:new(data.player);
 		local message = data.message;
 		
